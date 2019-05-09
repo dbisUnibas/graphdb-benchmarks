bf5e5da1d568fb89ab456bc1ef88200f513738d6
FUUbi <fabriparrillo@hotmail.com>
2019-05-09 16:02:16 +0200

    Update Neo4j from version 2.1.8 to 3.5.4 (#4)

--- src/main/java/eu/socialsensor/insert/Neo4jSingleInsertion.java
+++ src/main/java/eu/socialsensor/insert/Neo4jSingleInsertion.java
@@ -4,15 +4,14 @@ package eu.socialsensor.insert;
 import eu.socialsensor.graphdatabases.Neo4jGraphDatabase;
 import eu.socialsensor.main.BenchmarkingException;
 import eu.socialsensor.main.GraphDatabaseType;
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-import org.neo4j.cypher.javacompat.ExecutionEngine;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.ResourceIterator;
 import org.neo4j.graphdb.Transaction;
-import org.neo4j.kernel.GraphDatabaseAPI;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -25,25 +24,23 @@ import org.neo4j.kernel.GraphDatabaseAPI;
 public class Neo4jSingleInsertion extends InsertionBase<Node> {
 
     private final GraphDatabaseService neo4jGraph;
-    private final ExecutionEngine engine;
 
 
     public Neo4jSingleInsertion( GraphDatabaseService neo4jGraph, File resultsPath ) {
         super( GraphDatabaseType.NEO4J, resultsPath );
         this.neo4jGraph = neo4jGraph;
-        engine = new ExecutionEngine( this.neo4jGraph );
     }
 
 
     public Node getOrCreate( String nodeId ) {
         Node result;
 
-        try ( final Transaction tx = ((GraphDatabaseAPI) neo4jGraph).tx().unforced().begin() ) {
+        try ( final Transaction tx = neo4jGraph.beginTx() ) {
             try {
                 String queryString = "MERGE (n:Node {nodeId: {nodeId}}) RETURN n";
                 Map<String, Object> parameters = new HashMap<>();
                 parameters.put( "nodeId", nodeId );
-                ResourceIterator<Node> resultIterator = engine.execute( queryString, parameters ).columnAs( "n" );
+                ResourceIterator<Node> resultIterator = neo4jGraph.execute( queryString, parameters ).columnAs( "n" );
                 result = resultIterator.next();
                 tx.success();
             } catch ( Exception e ) {
@@ -58,7 +55,7 @@ public class Neo4jSingleInsertion extends InsertionBase<Node> {
 
     @Override
     public void relateNodes( Node src, Node dest ) {
-        try ( final Transaction tx = ((GraphDatabaseAPI) neo4jGraph).tx().unforced().begin() ) {
+        try ( final Transaction tx = neo4jGraph.beginTx() ) {
             try {
                 src.createRelationshipTo( dest, Neo4jGraphDatabase.RelTypes.SIMILAR );
                 tx.success();
