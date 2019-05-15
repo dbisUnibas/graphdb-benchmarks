88c6a4ade8c1248d707153a8282a45c6b64d2f24
gabuzi <gabriel.zihlmann@outlook.com>
2019-05-15 21:06:25 +0200

    fixed insertion tests after changed from str to int

--- src/test/java/eu/socialsensor/insert/TinkerPopInsertionTestCases.java
+++ src/test/java/eu/socialsensor/insert/TinkerPopInsertionTestCases.java
@@ -13,26 +13,26 @@ public class TinkerPopInsertionTestCases {
 
         assertEquals(0, (long) graph.traversal().V().count().next());
         Vertex v;
-        v = ins.getOrCreate("test");
+        v = ins.getOrCreate("0");
         assertEquals(1, (long) graph.traversal().V().count().next());
-        assertEquals("test", v.property(GraphDatabaseBase.NODE_ID).value());
-        v = ins.getOrCreate("test");
+        assertEquals(0, v.property(GraphDatabaseBase.NODE_ID).value());
+        v = ins.getOrCreate("0");
         assertEquals(1, (long) graph.traversal().V().count().next());
-        assertEquals("test", v.property(GraphDatabaseBase.NODE_ID).value());
-        v = ins.getOrCreate("test2");
+        assertEquals(0, v.property(GraphDatabaseBase.NODE_ID).value());
+        v = ins.getOrCreate("1");
         assertEquals(2, (long) graph.traversal().V().count().next());
-        assertEquals("test2", v.property(GraphDatabaseBase.NODE_ID).value());
-        v = ins.getOrCreate("test2");
+        assertEquals(1, v.property(GraphDatabaseBase.NODE_ID).value());
+        v = ins.getOrCreate("1");
         assertEquals(2, (long) graph.traversal().V().count().next());
-        assertEquals("test2", v.property(GraphDatabaseBase.NODE_ID).value());
+        assertEquals(1, v.property(GraphDatabaseBase.NODE_ID).value());
     }
 
     static void relateNodes(Graph graph, InsertionBase<Vertex> ins) {
         assertEquals(0, (long) graph.traversal().E().count().next());
 
         assertEquals(0, (long) graph.traversal().E().count().next());
-        Vertex v1 = ins.getOrCreate("test");
-        Vertex v2 = ins.getOrCreate("test2");
+        Vertex v1 = ins.getOrCreate("0");
+        Vertex v2 = ins.getOrCreate("1");
         ins.relateNodes(v1, v2);
         assertEquals(1, (long) graph.traversal().E().count().next());
         ins.relateNodes(v1, v2);
