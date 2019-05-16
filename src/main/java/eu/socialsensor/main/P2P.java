65eb9b19c239ff1abd975b62c8a5cdca721b37b4
Fuubi <fabriparrillo@hotmail.com>
2019-05-16 09:38:18 +0200

    Clean up remove test activity

--- src/main/java/eu/socialsensor/main/P2P.java
+++ src/main/java/eu/socialsensor/main/P2P.java
@@ -1,15 +1,9 @@
 package eu.socialsensor.main;
 
-import eu.socialsensor.graphdatabases.hypergraph.vertex.Node;
-import eu.socialsensor.graphdatabases.hypergraph.vertex.NodeQueries;
-import org.hypergraphdb.HGHandle;
 import org.hypergraphdb.peer.HyperGraphPeer;
-import org.hypergraphdb.peer.cact.QueryCount;
 import org.hypergraphdb.util.HGUtils;
 
 import java.io.File;
-import java.util.Objects;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 public class P2P {
@@ -24,37 +18,6 @@ public class P2P {
 
     System.out.println("Connected peers to " + peer.getConfiguration().at("interfaceConfig").at("user"));
     peer.getConnectedPeers().forEach(System.out::println);
-    Node n = new Node(1, 1,1);
-    HGHandle testHandle = peer.getGraph().add(n);
-
-    // 2 seconds should be enough in a single machine to transfer the atom
-    try { Thread.sleep(2000); } catch (Throwable t) { }
-
-    peer.getConnectedPeers().forEach(id ->
-            peer.getActivityManager().initiateActivity(
-                    new QueryCount(peer, NodeQueries.nodeType(), id),
-                    result -> {
-              System.out.println("Activity " + result.getActivity().getId() + " finished.");
-                      System.out.println(((QueryCount)result.getActivity()).getResult());
-              if (result.getException() != null)
-                System.out.println("With exception: " + result.getException());
-            }));
-
-    Long count =  peer.getConnectedPeers().stream().map(id -> peer.getActivityManager().initiateActivity(new QueryCount(peer, NodeQueries.nodeType(), id)))
-            .map(activityResultFuture -> {
-                try {
-                    return activityResultFuture.get();
-                } catch (InterruptedException | ExecutionException e) {
-                    e.printStackTrace();
-                }
-                return null;
-            }).filter(Objects::nonNull)
-            .map(a -> (QueryCount) a.getActivity())
-            .map(QueryCount::getResult).reduce((a, b) -> a + b).get();
-
-    System.out.println(count);
-    Thread.sleep(5000);
-
 
     while (true)
       try { Thread.sleep(5000); } catch (InterruptedException ex) { break; }
